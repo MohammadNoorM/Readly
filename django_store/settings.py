26c4f18ff8f705f709b5e9987e7dc776fc4195aa
mohammad noor <mohammadnoormerai@gmail.com>
2026-03-07 15:19:06 +0100

    hide django secret key

--- django_store/settings.py
+++ django_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-74%x8gpw%mswwola795tk(9wof&0u^#^h)m*^3%mp98-toym_)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
